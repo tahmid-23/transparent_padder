fc03ffbcd86a683fe4fca38286d165a8953fd8ea
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-05-21 15:53:45 -0400

    center exact

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,6 @@ use image::{GenericImage, ImageBuffer};
 struct Args {
     #[clap(parse(from_os_str))]
     input: PathBuf,
-
-    #[clap(short, long, default_value_t = 250)]
-    width_padding: u32,
-
-    #[clap(short, long, default_value_t = 250)]
-    height_padding: u32
 }
 
 fn main() {
@@ -21,13 +15,20 @@ fn main() {
     let input_image = image::open(&args.input)
         .expect("Failed to parse image!");
 
-    let width_padding = args.width_padding;
-    let height_padding = args.height_padding;
-    let new_width = input_image.width() + 2 * width_padding;
-    let new_height = input_image.height() + 2 * height_padding;
+    let image_width = input_image.width();
+    let image_height = input_image.height();
+
+    let diameter_squared = image_width * image_width + image_height * image_height;
+    let diameter = (diameter_squared as f32).sqrt();
+
+    let delta_width = ((diameter - image_width as f32) / 2f32).ceil() as u32;
+    let delta_height = ((diameter - image_height as f32) / 2f32).ceil() as u32;
+
+    let new_width = image_width + 2 * delta_width;
+    let new_height = image_height + 2 * delta_height;
 
     let mut output_image = ImageBuffer::new(new_width, new_height);
-    output_image.copy_from(&input_image, width_padding, height_padding)
+    output_image.copy_from(&input_image, delta_width, delta_height)
         .expect("Could not copy image!");
 
     output_image.save("result.png").expect("Could not save image!");
